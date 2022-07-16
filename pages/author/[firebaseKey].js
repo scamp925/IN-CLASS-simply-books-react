ccc1ae42b5a369bcb6734d2e2fc65c37aa3d01f7
Sariah Campopiano <scampopiano925@gmail.com>
2022-07-16 10:12:36 -0500

    Created two dyncamic routing files for authors. One for viewing authors and the other for editing authors

---
+++ pages/author/[firebaseKey].js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function ViewAuthor() {
+  return (
+    <div>
+      Author Details
+    </div>
+  );
+}
---
+++ pages/author/edit/[firebaseKey].js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function EditAuthor() {
+  return (
+    <div>
+      Edit Author
+    </div>
+  );
+}
