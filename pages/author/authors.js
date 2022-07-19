6c839d0ac12ba69c59fb173442394f6426f3d4f3
Sariah Campopiano <scampopiano925@gmail.com>
2022-07-18 19:31:38 -0500

    Author cards render when user clicks authors in the navbar

--- pages/author/authors.js
+++ pages/author/authors.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Link from 'next/link';
+import { Button } from 'react-bootstrap';
+import { getAuthors } from '../../api/authorData';
 import AuthorCard from '../../components/AuthorCard';
+import { useAuth } from '../../utils/context/authContext';
 
 export default function Authors() {
+  const [authors, setAuthors] = useState([]);
+  const { user } = useAuth();
+
+  useEffect(() => {
+    getAuthors(user.uid).then(setAuthors);
+  }, [user]);
+
   return (
-    <div>
-      <AuthorCard />
+    <div className="text-center my-4">
+      <Link href="/author/new" passHref>
+        <Button>Add An Author</Button>
+      </Link>
+      <div className="d-flex flex-wrap">
+        {authors.map((author) => (
+          <AuthorCard key={author.firebaseKey} firstName={author.first_name} lastName={author.last_name} email={author.email} />
+        ))}
+      </div>
     </div>
   );
 }
