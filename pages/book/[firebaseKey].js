fa7068926c56c4264669bff708525d98c01ff1e2
Sariah Campopiano <scampopiano925@gmail.com>
2022-07-20 14:37:30 -0500

    Cleaned up some uncommented code

--- pages/book/[firebaseKey].js
+++ pages/book/[firebaseKey].js
@@ -7,10 +7,10 @@ export default function ViewBook() {
   const [bookDetails, setBookDetails] = useState({});
   const router = useRouter();
 
-  // TODO: grab firebaseKey from url
+  // grab firebaseKey from url
   const { firebaseKey } = router.query;
 
-  // TODO: make call to API layer to get the data
+  // make call to API layer to get the data
   useEffect(() => {
     viewBookDetails(firebaseKey).then(setBookDetails);
   }, [firebaseKey]);
